a2c70f57203593f563a5bf5fdb94b22c4f300e0e
saki ota <saki.ota01@outlook.com>
2022-05-05 23:44:05 +1000

    do not push

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-
+# seed below is jsut for testing activities_index page purpose
 organisation1 = Organisation.create(name: "Organisation1")
-activity1 = Activity.create(name: "walk dog", description:"walk dogs for rich people", organisation: organisation1)
-activity2 = Activity.create(name: "life saver", description:"watch people if they are swimming between flags", organisation: organisation1)
+activity1 = Activity.create(name: "dog sitter", description:"look after dogs for rich people", organisation: organisation1)
+activity2 = Activity.create(name: "life saver", description:"watch people swimming between flags", organisation: organisation1)
+
+# *****************************************************************
