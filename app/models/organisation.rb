c8565c2b7624df1a4a3e8e001c7b80eb10da2bea
saki ota <saki.ota01@outlook.com>
2022-05-05 23:51:51 +1000

    dont ppush

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,10 +1,4 @@
 class Organisation < ApplicationRecord
   has_many :activities, dependent: :destroy
   validates :name, presence: true
-
-  # if params[:query].present?
-  #   @organisations = PgSearch.multisearch(params[:query]).map(&:searchable)
-  # else
-  #   @organisations = Organisation.all
-  # end
 end
