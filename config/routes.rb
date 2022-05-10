0f0d4a9039d8622f9d82c8c468e658a5b880c089
saki ota <saki.ota01@outlook.com>
2022-05-10 21:08:14 +1000

    added contents in profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'profile', to: 'users#profile'
   # Front page shows the options
   root to: 'pages#home'
 
